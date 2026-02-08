f9be66cbf7007db5dd81bfb454211a257c50520f
Chan <mychan0926@gmail.com>
2026-02-08 19:45:28 +0900

    feat: 싱글톤 패턴 구현 및 기본 스크립트 파일 생성

---
+++ Assets/02.Scripts/Utils/Singleton.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance != null)
+                return _instance;
+
+            _instance = FindFirstObjectByType<T>();
+
+            if (_instance == null)
+            {
+                GameObject obj = new GameObject(typeof(T).Name);
+                _instance = obj.AddComponent<T>();
+            }
+
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this as T;
+        
+        if (transform.parent != null)
+            DontDestroyOnLoad(transform.root.gameObject);
+        else
+            DontDestroyOnLoad(gameObject);
+    }
+}
